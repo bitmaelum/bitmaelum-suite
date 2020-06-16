95aa94d1bcd72edd3a495ce2ea74c52a049b7981
Joshua Thijssen <jthijssen@noxlogic.nl>
2020-06-16 10:14:55 +0200

    Added documentation directory

--- create-cert.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#
-# Create a self-signed certificate for the mailserver.
-#
-# Usage: ./create-cert.sh <my.server.tld>
-#
-
-openssl req -x509 -newkey rsa:2048 -keyout server.key -out server.crt -days 3650 -nodes -subj "/CN=${1}"
