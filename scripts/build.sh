db7c898cf0bf2bd0420db8bfc0b1fb5bbe2a432c
Joshua Thijssen <jthijssen@noxlogic.nl>
2020-06-17 10:53:31 +0200

    Using a more generic printf instead of echo

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,19 +10,19 @@ go get github.com/ahmetb/govvv
 GO_PATH=`go env GOPATH`
 GO_BUILD_FLAGS=`${GO_PATH}/bin/govvv build -pkg version -flags`
 
-echo "Compiling [\c"
+printf "Compiling ["
 
-echo ".\c"
+printf  "."
 go build -ldflags "${GO_BUILD_FLAGS}" -o release/bm-server ${REPO}/bm-server
-echo ".\c"
+printf  "."
 go build -ldflags "${GO_BUILD_FLAGS}" -o release/bm-config ${REPO}/bm-config
-echo ".\c"
+printf  "."
 go build -ldflags "${GO_BUILD_FLAGS}" -o release/bm-client ${REPO}/bm-client-ui
-echo ".\c"
+printf  "."
 
 for TOOL in $TOOLS; do
   go build -ldflags "${GO_BUILD_FLAGS}" -o release/${TOOL} ${REPO}/tools/${TOOL}
-  echo ".\c"
+  printf "."
 done
 
-echo "]"
+printf "]\n"
