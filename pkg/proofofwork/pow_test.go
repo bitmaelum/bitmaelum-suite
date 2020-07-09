cb2716e570ba4c452dad13a9b4e38733f9cd9a7d
Joshua Thijssen <jthijssen@noxlogic.nl>
2020-07-09 11:44:33 +0200

    cleanup

--- pkg/proofofwork/pow_test.go
+++ pkg/proofofwork/pow_test.go
@@ -10,16 +10,16 @@ func Test_ProofOfWork(t *testing.T) {
 	assert.Equal(t, 8, pow.Bits)
 	assert.Equal(t, uint64(0), pow.Proof)
 	assert.False(t, pow.HasDoneWork())
-	assert.False(t, pow.Validate())
+	assert.False(t, pow.IsValid())
 
 	pow.Work()
 	assert.True(t, pow.HasDoneWork())
-	assert.True(t, pow.Validate())
+	assert.True(t, pow.IsValid())
 	assert.Equal(t, uint64(88), pow.Proof)
 
 	pow = New(8, []byte("jane@example!"), 171)
 	assert.Equal(t, 8, pow.Bits)
 	assert.Equal(t, uint64(171), pow.Proof)
 	assert.True(t, pow.HasDoneWork())
-	assert.True(t, pow.Validate())
+	assert.True(t, pow.IsValid())
 }
