47cf54c9c3e723a28eddc96ff573cb308b755062
Joshua Thijssen <jthijssen@noxlogic.nl>
2020-07-16 11:06:21 +0200

    Moved most of letsencrypt to separate package

--- cmd/bm-config/cmd/letsEncrypt.go
+++ cmd/bm-config/cmd/letsEncrypt.go
@@ -1,38 +1,15 @@
 package cmd
 
 import (
-	"context"
-	"crypto"
-	"crypto/ecdsa"
-	"crypto/elliptic"
-	"crypto/rand"
-	"crypto/rsa"
-	"crypto/x509"
-	"crypto/x509/pkix"
-	"encoding/json"
-	"encoding/pem"
-	"errors"
 	"fmt"
-	"github.com/bitmaelum/bitmaelum-suite/internal"
+	"github.com/bitmaelum/bitmaelum-suite/cmd/bm-config/internal/fileio"
+	"github.com/bitmaelum/bitmaelum-suite/cmd/bm-config/internal/letsencrypt"
 	"github.com/bitmaelum/bitmaelum-suite/internal/config"
-	"github.com/bitmaelum/bitmaelum-suite/internal/encrypt"
-	"github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
-	"golang.org/x/crypto/acme"
-	"io/ioutil"
-	"net"
-	"net/http"
 	"os"
 	"path"
 	"strconv"
-	"strings"
-	"syscall"
 	"time"
-)
-
-var (
-	ctx        context.Context
-	acmeClient *acme.Client
 )
 
 // letsEncryptCmd represents the lets encrypt  command
@@ -57,79 +34,107 @@ certificate and key in the paths you defined in your config.`,
 			os.Exit(1)
 		}
 
-		registerLetsEncrypt(*staging, *port)
+		do(*staging, *port)
 	},
 }
 
-func registerLetsEncrypt(useStaging bool, httpPort string) {
-	var (
-		err    error
-		domain = config.Server.Server.Name
-	)
-
-	// Check if we are renewing an existing certificate or not
-	var cert, renewalMode = checkRenewal()
-	if cert != nil && renewalMode == false {
-		fmt.Println(" * Domain does not need to be renewed yet.")
-		os.Exit(0)
-	}
-
-	// Create context
-	var cancel context.CancelFunc
-	ctx, cancel = context.WithTimeout(context.Background(), 1*time.Minute)
-	defer cancel()
-
-	// Create client
-	fmt.Printf(" * Trying to setup the connection to LetsEncrypt...")
-	acmeClient, err = getClient(useStaging)
+func do(useStaging bool, httpPort string) {
+	// 1.  Get or register ACME account
+	fmt.Printf(" * Fetching account from your ACME directory...")
+	le, err := letsencrypt.New(httpPort, useStaging, getAcmePath(useStaging))
 	if err != nil {
-		fmt.Println("Cannot connect to LetsEncrypt")
+		fmt.Printf("An error occurred: %s\n", err)
 		os.Exit(1)
 	}
-	fmt.Println("ok")
+	defer le.Cancel()
 
-	// Starting HTTP server for validation
+	if le.Account == nil {
+		fmt.Println("not found")
+
+		fmt.Printf(" * Registering a new account at LetsEncrypt...")
+		err := le.RegisterAccount(config.Server.Acme.Email)
+		if err != nil {
+			fmt.Printf("An error occurred: %s\n", err)
+			os.Exit(1)
+		}
+		fmt.Println("ok")
+
+		// Save into account directory. We don't care if it fails
+		fmt.Printf(" * Saving account info in your ACME directory...")
+		err = le.SaveAccount(getAcmePath(useStaging))
+		if err != nil {
+			fmt.Printf("An error occurred: %s\n", err)
+			os.Exit(1)
+		}
+		fmt.Println("ok")
+
+	} else {
+		fmt.Println("found")
+	}
+
+	// 2.  Check if we are renewing an existing certificate or not
+	fmt.Printf(" * Checking current certificate in \"%s\"...", config.Server.Server.CertFile)
+	cert := le.LoadCertificate(config.Server.Server.CertFile)
+	if cert == nil {
+		fmt.Println("not found. That's ok")
+	} else {
+		fmt.Println("found")
+
+		if cert.Subject.CommonName != config.Server.Acme.Domain {
+			fmt.Printf(" * Domain found in certificate does not match configured name (want: %s, got: %s)\n", config.Server.Acme.Domain, cert.Subject.CommonName)
+			os.Exit(1)
+		}
+
+		days, err := strconv.Atoi(config.Server.Acme.RenewBeforeDays)
+		if err != nil {
+			days = 30
+		}
+
+		if le.CheckRenewal(cert, days) {
+			fmt.Println(" * Domain does not need to be renewed yet.")
+			os.Exit(0)
+		}
+		fmt.Printf(" * Domain \"%s\" valid until %s\n", cert.Subject.CommonName, cert.NotAfter.Format(time.RFC822))
+	}
+
+	// 3.  Starting HTTP server for validation
 	fmt.Printf(" * Starting up the HTTP server...")
-	server := startHTTPServer(httpPort)
+	server := le.StartHTTPServer()
 	defer func() {
 		_ = server.Close()
 	}()
 	fmt.Println("up")
 
-	// Register or fetch account and private key, and set it in our acme client
-	accountPrivKey, _, err := getOrRegisterAccount(config.Server.Acme.Email)
-	acmeClient.Key = accountPrivKey
-
-	// 1.  Submit an order for a certificate to be issued
+	// 4.  Submit an order for a certificate to be issued
 	fmt.Printf(" * Creating an order at LetsEncrypt for our domain...")
-	order, err := acmeClient.AuthorizeOrder(ctx, acme.DomainIDs(domain))
+	order, err := le.AuthorizeOrder(config.Server.Acme.Domain)
 	if err != nil {
 		fmt.Println("An error occurred while trying to register your domain at LetsEncrypt")
 		os.Exit(1)
 	}
 	fmt.Println("ok")
 
-	// 2.  Prove control of any identifiers requested in the certificate
+	// 5.  Prove control of any identifiers requested in the certificate
 	fmt.Printf(" * Waiting until LetsEncrypt calls our webserver...")
-	order, err = authorize(order)
+	order, err = le.Authorize(order)
 	if err != nil {
 		fmt.Printf("An error occurred while trying to authorize your domain at LetsEncrypt: %s\n", err)
 		os.Exit(1)
 	}
 	fmt.Println("ok")
 
-	// 3.  Finalize the order by submitting a CSR
+	// 6.  Finalize the order by submitting a CSR
 	fmt.Printf(" * Verification complete. Asking for your certificate...")
-	privKeyPem, certPem, err := finalizeOrder(order, domain, nil)
+	privKeyPem, certPem, err := le.FinalizeOrder(order, config.Server.Acme.Domain, nil)
 	if err != nil {
 		fmt.Printf("Error while fetching certificates: %s\n", err)
 		os.Exit(1)
 	}
 	fmt.Println("ok")
 
-	// 4. Save the files
+	// 7. Save the files
 	fmt.Println(" * Writing files:")
-	err = saveCertFiles(certPem, privKeyPem)
+	err = fileio.SaveCertFiles(certPem, privKeyPem)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -138,363 +143,11 @@ func registerLetsEncrypt(useStaging bool, httpPort string) {
 	fmt.Printf("\nAll done. Reload your BitMaelum server and you are good to go!\n")
 }
 
-func saveCertFiles(certPem string, keyPem string) error {
-	// Find the latest suffix we can use that both certfile and keyfile do not use.
-	var suffix = 1
-	for {
-		p, _ := homedir.Expand(fmt.Sprintf("%s.%03d", config.Server.Server.CertFile, suffix))
-		_, err1 := os.Stat(p)
-		p, _ = homedir.Expand(fmt.Sprintf("%s.%03d", config.Server.Server.KeyFile, suffix))
-		_, err2 := os.Stat(p)
-		if err1 == nil || err2 == nil {
-			suffix++
-			continue
-		}
-		break
-	}
-
-	newPath, _ := homedir.Expand(fmt.Sprintf("%s.%03d", config.Server.Server.CertFile, suffix))
-	oldPath, _ := homedir.Expand(config.Server.Server.CertFile)
-	fmt.Printf("   - moving old cert file to %s: ", newPath)
-	err := os.Rename(oldPath, newPath)
-	if err != nil {
-		return err
-	}
-	fmt.Println("ok")
-
-	newPath, _ = homedir.Expand(fmt.Sprintf("%s.%03d", config.Server.Server.KeyFile, suffix))
-	oldPath, _ = homedir.Expand(config.Server.Server.KeyFile)
-	fmt.Printf("   - moving old key file to %s: ", newPath)
-	err = os.Rename(oldPath, newPath)
-	if err != nil {
-		return err
-	}
-	fmt.Println("ok")
-
-	fmt.Printf("   - Writing new cert file %s: ", config.Server.Server.CertFile)
-	newPath, _ = homedir.Expand(config.Server.Server.CertFile)
-	err = ioutil.WriteFile(newPath, []byte(certPem), 0600)
-	if err != nil {
-		return err
-	}
-	fmt.Println("ok")
-
-	fmt.Printf("   - Writing new key file %s: ", config.Server.Server.CertFile)
-	newPath, _ = homedir.Expand(config.Server.Server.KeyFile)
-	err = ioutil.WriteFile(newPath, []byte(keyPem), 0600)
-	if err != nil {
-		return err
-	}
-	fmt.Println("ok")
-
-	return nil
-}
-
-func getOrRegisterAccount(email string) (crypto.Signer, *acme.Account, error) {
-	// 0. Load or register account
-	fmt.Printf(" * Fetching account from your ACME directory...")
-	accountPrivKey, acc, err := getAccountFromAcmeDir()
-	if err == nil {
-		fmt.Println("found")
-
-		return accountPrivKey, acc, nil
-	}
-	fmt.Println("not found.")
-
-	// Saved acme account not found. Register a new one..
-	fmt.Printf(" * Registering your email at LetsEncrypt...")
-	accountPrivKey, acc, err = registerAccount(email)
-	if err != nil {
-		fmt.Println("An error occurred while trying to register your email address. Maybe it's incorrect?")
-		os.Exit(1)
-	}
-	fmt.Println("ok")
-
-	// Save into account directory. We don't care if it fails
-	fmt.Printf(" * Saving account in your ACME directory...")
-	_ = saveAccount(acc, accountPrivKey)
-	fmt.Println("ok")
-
-	return accountPrivKey, acc, nil
-}
-
-func checkRenewal() (*x509.Certificate, bool) {
-	fmt.Printf(" * Checking current certificate in \"%s\"...", config.Server.Server.CertFile)
-	cert := fetchCertificate()
-	if cert == nil {
-		fmt.Println("not found.")
-		return nil, false
-	}
-
-	fmt.Println("found")
-
-	host, _, _ := net.SplitHostPort(config.Server.Server.Name)
-	if host == "" {
-		host = config.Server.Server.Name
-	}
-
-	if cert.Subject.CommonName != host {
-		fmt.Printf(" * Domain found in certificate does not match configured name (want: %s, got: %s)\n", host, cert.Subject.CommonName)
-		os.Exit(1)
-	}
-
-	fmt.Printf(" * Domain \"%s\" valid until %s\n", cert.Subject.CommonName, cert.NotAfter.Format(time.RFC822))
-	days, err := strconv.Atoi(config.Server.Acme.RenewBeforeDays)
-	if err != nil {
-		days = 30
-	}
-	if cert.NotAfter.Unix() > time.Now().Add(time.Duration(days*24)*time.Hour).Unix() {
-		return cert, false
-	}
-
-	return cert, true
-}
-
-func fetchCertificate() *x509.Certificate {
-	p, err := homedir.Expand(config.Server.Server.CertFile)
-	if err != nil {
-		return nil
-	}
-
-	data, err := ioutil.ReadFile(p)
-	if err != nil {
-		return nil
-	}
-
-	// Decode FIRST certificate found in the certfile
-	var certDERBlock *pem.Block
-	certDERBlock, data = pem.Decode(data)
-	cert, err := x509.ParseCertificates(certDERBlock.Bytes)
-	if err != nil || len(cert) == 0 {
-		return nil
-	}
-
-	return cert[0]
-}
-
-func finalizeOrder(order *acme.Order, domain string, privCertKey crypto.Signer) (privKey string, certificate string, err error) {
-	// Generate a new private key if not supplied
-	if privCertKey == nil {
-		privCertKey, err = rsa.GenerateKey(rand.Reader, 2048)
-	}
-
-	// Create CSR
-	req := &x509.CertificateRequest{
-		Subject: pkix.Name{CommonName: domain},
-	}
-	csr, err := x509.CreateCertificateRequest(rand.Reader, req, privCertKey)
-	if err != nil {
-		return "", "", err
-	}
-
-	// Let LetsEncrypt sign the CSR and return the certificate
-	certs, _, err := acmeClient.CreateOrderCert(ctx, order.FinalizeURL, csr, false)
-	if err != nil {
-		return "", "", err
-	}
-
-	// Convert all certificates to PEM format
-	for _, cert := range certs {
-		c, err := x509.ParseCertificate(cert)
-		if err != nil {
-			return "", "", err
-		}
-
-		certPem, err := encrypt.CertToPEM(*c)
-		certificate += certPem
-	}
-
-	// Convert private and public key to PEM
-	privKey, err = encrypt.PrivKeyToPEM(privCertKey)
-	if err != nil {
-		return "", "", err
-	}
-
-	return
-}
-
-func authorize(order *acme.Order) (*acme.Order, error) {
-	for _, authzURL := range order.AuthzURLs {
-		auth, err := acmeClient.GetAuthorization(ctx, authzURL)
-		if err != nil {
-			continue
-		}
-
-		for _, challenge := range auth.Challenges {
-			if challenge.Type == "http-01" {
-				order, err := acceptChallenge(ctx, challenge, order)
-				if err != nil {
-					continue
-				}
-
-				return order, nil
-			}
-		}
-	}
-
-	return nil, errors.New("cannot find a authorization/challenge")
-}
-
-func acceptChallenge(ctx context.Context, challenge *acme.Challenge, order *acme.Order) (*acme.Order, error) {
-	// Do this challenge
-	_, err := acmeClient.Accept(ctx, challenge)
-	if err != nil {
-		return nil, err
-	}
-
-	// Wait until order is completed
-	order, err = acmeClient.WaitOrder(ctx, order.URI)
-	if err != nil {
-		return nil, err
-	}
-
-	if order.Status != acme.StatusReady {
-		return nil, errors.New("validation seems to have failed")
-	}
-
-	return order, nil
-}
-
-func registerAccount(email string) (crypto.Signer, *acme.Account, error) {
-	// Create public/private key pair.
-	key, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
-	if err != nil {
-		return nil, nil, err
-	}
-	// Needed to set here so acmeClient.register() will work
-	acmeClient.Key = key
-
-	acc := &acme.Account{
-		Contact: []string{"mailto:" + email},
-	}
-	acc, err = acmeClient.Register(ctx, acc, acme.AcceptTOS)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	return key, acc, nil
-}
-
-func saveAccount(acc *acme.Account, key crypto.Signer) error {
-	// @TODO: What happens if we mix staging and production.. or different accounts?
-	err := saveFile(config.Server.Acme.Path+"/account.json", acc)
-	if err != nil {
-		return err
-	}
-
-	// Convert key to PEM format
-	s, err := encrypt.PrivKeyToPEM(key)
-	if err != nil {
-		return err
-	}
-
-	return saveFile(config.Server.Acme.Path+"/key.json", s)
-}
-
-func saveFile(p string, v interface{}) error {
-	data, err := json.Marshal(v)
-	if err != nil {
-		return err
-	}
-
-	p, err = homedir.Expand(p)
-	if err != nil {
-		return err
-	}
-	err = os.MkdirAll(path.Dir(p), 755)
-	if err != nil {
-		return err
-	}
-
-	return ioutil.WriteFile(p, data, 0600)
-}
-
-func getClient(useStaging bool) (*acme.Client, error) {
-	// Use staging environment if needed
-	var acmeDir = acme.LetsEncryptURL
+func getAcmePath(useStaging bool) string {
 	if useStaging {
-		acmeDir = "https://acme-staging-v02.api.letsencrypt.org/directory"
+		return path.Join(config.Server.Acme.Path, "staging")
 	}
-
-	client := &acme.Client{
-		DirectoryURL: acmeDir,
-		UserAgent:    "BitMaelum v" + internal.Version.String(),
-	}
-
-	return client, nil
-}
-
-func startHTTPServer(port string) *http.Server {
-	// Setup simple web-server that listens to tokens and returns back.. things
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Printf(".")
-		// fmt.Printf("   - Incoming request: '%s'\n", r.URL.String())
-
-		token := strings.TrimPrefix(r.URL.String(), "/.well-known/acme-challenge/")
-		// fmt.Printf("   - Found token: '%s'\n", token)
-		response, err := acmeClient.HTTP01ChallengeResponse(token)
-		// fmt.Printf("   - Sending response: '%s'\n", response)
-		if err != nil {
-			w.WriteHeader(http.StatusNotFound)
-			return
-		}
-
-		w.WriteHeader(http.StatusOK)
-		_, _ = w.Write([]byte(response))
-	})
-
-	server := &http.Server{Addr: ":" + port}
-	go func() {
-		err := server.ListenAndServe()
-		if err != nil && err != http.ErrServerClosed {
-			fmt.Printf("We cannot start a HTTP server on port %s on your machine.\n", port)
-			if err.(*net.OpError).Err.(*os.SyscallError).Err == syscall.EADDRINUSE {
-				fmt.Printf("It seems there is already a server running on port %s.\n", port)
-			} else if err.(*net.OpError).Err.(*os.SyscallError).Err == syscall.EACCES {
-				fmt.Printf("It seems you are not running as root/Administrator, which is needed to use port %s.\n", port)
-			} else {
-				fmt.Printf("An unknown error has occurred: %s\n", err)
-			}
-			os.Exit(1)
-		}
-	}()
-
-	return server
-}
-
-func getAccountFromAcmeDir() (crypto.Signer, *acme.Account, error) {
-	acc := &acme.Account{}
-	err := loadFile(config.Server.Acme.Path+"/account.json", &acc)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	// can't marshal ecdsa.PrivKey to JSON (go 1.16+ probably)
-	var data string
-	err = loadFile(config.Server.Acme.Path+"/key.json", &data)
-	if err != nil {
-		return nil, nil, err
-	}
-	key, err := encrypt.PEMToPrivKey([]byte(data))
-	if err != nil {
-		return nil, nil, err
-	}
-
-	return key.(crypto.Signer), acc, nil
-}
-
-func loadFile(p string, v interface{}) error {
-	p, err := homedir.Expand(p)
-	if err != nil {
-		return err
-	}
-
-	data, err := ioutil.ReadFile(p)
-	if err != nil {
-		return err
-	}
-
-	return json.Unmarshal(data, v)
+	return path.Join(config.Server.Acme.Path, "prod")
 }
 
 var (
