ad88359caa93015cca6574283e1ca885783ae51d
Joshua Thijssen <jthijssen@noxlogic.nl>
2020-07-26 16:23:37 +0200

    Guarding scoreboard with mutex

--- cmd/bm-server/processor/scoreboard.go
+++ cmd/bm-server/processor/scoreboard.go
@@ -1,20 +1,30 @@
 package processor
 
-var scoreboard = map[string]int{}
+import "sync"
+
+type scoreboardType struct {
+	mux sync.Mutex
+	v   map[string]int
+}
+var scoreboard scoreboardType
 
 // IsInScoreboard will check if the given message ID in the section is present
 func IsInScoreboard(section int, msgID string) bool {
-	s, found := scoreboard[msgID]
+	s, found := scoreboard.v[msgID]
 
 	return found && s == section
 }
 
 // AddToScoreboard will set the message ID in the section in the scoreboard
 func AddToScoreboard(section int, msgID string) {
-	scoreboard[msgID] = section
+	scoreboard.mux.Lock()
+	scoreboard.v[msgID] = section
+	scoreboard.mux.Unlock()
 }
 
 // RemoveFromScoreboard will remove the message ID from the section in the scoreboard
 func RemoveFromScoreboard(section int, msgID string) {
-	delete(scoreboard, msgID)
+	scoreboard.mux.Lock()
+	delete(scoreboard.v, msgID)
+	scoreboard.mux.Unlock()
 }
