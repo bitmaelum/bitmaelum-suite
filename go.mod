cc7ce9f71535807ef69bc6364bd967be4d55cb36
Antonio Calatrava <antonio@antoniocalatrava.com>
2020-10-16 21:58:41 +0200

    Add mnemonic determinist RSA key generation support

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.13
 require (
 	github.com/alicebob/gopher-json v0.0.0-20200520072559-a9ecdc9d1d3a // indirect
 	github.com/c2h5oh/datasize v0.0.0-20200112174442-28bbd4740fee
+	github.com/cloudflare/gokey v0.1.0
 	github.com/coreos/go-semver v0.3.0
 	github.com/davecgh/go-spew v1.1.1
 	github.com/elliotchance/redismock/v8 v8.1.0
--- pkg/bmcrypto/mnemonic.go
+++ pkg/bmcrypto/mnemonic.go
@@ -20,16 +20,68 @@
 package bmcrypto
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
 	"crypto/ed25519"
 	"crypto/sha256"
 	"io"
+	"strings"
 
+	deterministicRsaKeygen "github.com/cloudflare/gokey/rsa"
 	"github.com/tyler-smith/go-bip39"
 	"golang.org/x/crypto/hkdf"
 )
 
+//Taken from https://github.com/cloudflare/gokey/blob/6bb7290160583cf1fd7cdcb5726093a00dd23c25/csprng.go#L18
+type devZero struct{}
+
+func (dz devZero) Read(p []byte) (n int, err error) {
+	for i := range p {
+		p[i] = 0
+	}
+	return len(p), nil
+}
+
+// GenerateRSAKeypairFromMnemonic generates a keypair based on the given mnemonic
+func GenerateRSAKeypairFromMnemonic(mnemonic string) (*PrivKey, *PubKey, error) {
+	e, err := bip39.MnemonicToByteArray(mnemonic, true)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return genRSAKey(e)
+}
+
+// GenerateRSAKeypairWithMnemonic generates a mnemonic, and a keypair that can be generated through the same mnemonic again.
+func GenerateRSAKeypairWithMnemonic() (string, *PrivKey, *PubKey, error) {
+	// Generate large enough random string
+	e, err := bip39.NewEntropy(192)
+	if err != nil {
+		return "", nil, nil, err
+	}
+
+	// Generate Mnemonic words
+	mnemonic, err := bip39.NewMnemonic(e)
+	if err != nil {
+		return "", nil, nil, err
+	}
+
+	privKey, pubKey, err := genRSAKey(e)
+	if err != nil {
+		return "", nil, nil, err
+	}
+
+	return "rsa " + mnemonic, privKey, pubKey, nil
+}
+
 // GenerateKeypairFromMnemonic generates a keypair based on the given mnemonic
 func GenerateKeypairFromMnemonic(mnemonic string) (*PrivKey, *PubKey, error) {
+	// Check if it's a RSA mnemonic
+	words := strings.SplitN(mnemonic, " ", 2)
+	if strings.ToLower(words[0]) == "rsa" {
+		return GenerateRSAKeypairFromMnemonic(strings.Join(words[1:], " "))
+	}
+
 	e, err := bip39.MnemonicToByteArray(mnemonic, true)
 	if err != nil {
 		return nil, nil, err
@@ -58,6 +110,35 @@ func GenerateKeypairWithMnemonic() (string, *PrivKey, *PubKey, error) {
 	}
 
 	return mnemonic, privKey, pubKey, nil
+}
+
+func genRSAKey(e []byte) (*PrivKey, *PubKey, error) {
+	// Stretch 192 bits to 256 bits
+	rd := hkdf.New(sha256.New, e, []byte{}, []byte{})
+	expbuf := make([]byte, 32)
+	_, err := io.ReadFull(rd, expbuf)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// Generate keypair
+	// Taken from https://github.com/cloudflare/gokey/blob/6bb7290160583cf1fd7cdcb5726093a00dd23c25/csprng.go#L56
+	block, _ := aes.NewCipher(expbuf[:32])
+	stream := cipher.NewCTR(block, make([]byte, 16))
+
+	randReader := cipher.StreamReader{S: stream, R: devZero{}}
+	privRSAKey, err := deterministicRsaKeygen.GenerateKey(randReader, rsaBits)
+
+	privKey, err := NewPrivKeyFromInterface(privRSAKey)
+	if err != nil {
+		return nil, nil, err
+	}
+	pubKey, err := NewPubKeyFromInterface(privRSAKey.Public())
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return privKey, pubKey, nil
 }
 
 func genKey(e []byte) (*PrivKey, *PubKey, error) {
