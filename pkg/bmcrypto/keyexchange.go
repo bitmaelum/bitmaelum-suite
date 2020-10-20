0744daa9db32f55102d677d49e232f2d83161827
Joshua Thijssen <jthijssen@noxlogic.nl>
2020-10-20 18:09:25 +0200

    check sonar

--- pkg/bmcrypto/keyexchange.go
+++ pkg/bmcrypto/keyexchange.go
@@ -63,3 +63,37 @@ func EdPrivToX25519(privateKey ed25519.PrivateKey) []byte {
 	return digest[:32]
 }
 
+var curve25519P, _ = new(big.Int).SetString("57896044618658097711785492504343953926634992332820282019728792003956564819949", 10)
+
+// EdPubToX25519 converts a ed25519 Public Key to a X25519 Public Key
+func EdPubToX25519(pk ed25519.PublicKey) []byte {
+	// ed25519.PublicKey is a little endian representation of the y-coordinate,
+	// with the most significant bit set based on the sign of the x-coordinate.
+	bigEndianY := make([]byte, ed25519.PublicKeySize)
+	for i, b := range pk {
+		bigEndianY[ed25519.PublicKeySize-i-1] = b
+	}
+	bigEndianY[0] &= 0b0111_1111
+
+	/* The Montgomery u-coordinate is derived through the bilinear map
+	 *
+	 *     u = (1 + y) / (1 - y)
+	 *
+	 * See https://blog.filippo.io/using-ed25519-keys-for-encryption.
+	 */
+	// y := new(big.Int).SetBytes(bigEndianY)
+	denom := big.NewInt(1)
+	// denom.ModInverse(denom.Sub(denom, y), curve25519P) // 1 / (1 - y)
+	// u := y.Mul(y.Add(y, big.NewInt(1)), denom)
+	u := denom
+	u.Mod(u, curve25519P)
+
+
+	out := make([]byte, curve25519.PointSize)
+	uBytes := u.Bytes()
+	for i, b := range uBytes {
+		out[len(uBytes)-i-1] = b
+	}
+
+	return out
+}
