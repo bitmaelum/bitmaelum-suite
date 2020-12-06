4b699a6bd8711b6a5a6bf80c9120de705978e40b
Joshua Thijssen <jthijssen@noxlogic.nl>
2020-12-06 15:43:29 +0100

    display message box

---
+++ cmd/bm-json/cmd/box.go
@@ -0,0 +1,70 @@
+// Copyright (c) 2020 BitMaelum Authors
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+package cmd
+
+import (
+	"os"
+
+	"github.com/bitmaelum/bitmaelum-suite/cmd/bm-json/internal"
+	"github.com/bitmaelum/bitmaelum-suite/cmd/bm-json/internal/output"
+	"github.com/spf13/cobra"
+)
+
+var boxCmd = &cobra.Command{
+	Use:   "box",
+	Short: "Returns message details",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		// Get generic structs
+		_, info, client, err := internal.GetClientAndInfo(*boxAccount)
+		if err != nil {
+			output.JSONErrorOut(err)
+			os.Exit(1)
+		}
+
+		mbl, err := client.GetMailboxList(info.Address.Hash())
+		if err != nil {
+			output.JSONErrorOut(err)
+			os.Exit(1)
+		}
+
+		var out []output.JSONT
+		for _, mb := range mbl.Boxes {
+			out = append(out, output.JSONT{
+				"id":       mb.ID,
+				"total": mb.Total,
+				"messages": mb.Messages,
+			})
+		}
+
+		output.JSONOut(out)
+	},
+}
+
+var (
+	boxAccount *string
+)
+
+func init() {
+	rootCmd.AddCommand(boxCmd)
+
+	boxAccount = boxCmd.Flags().StringP("account", "a", "", "Account")
+	_ = boxCmd.MarkFlagRequired("account")
+}
